8870ec26b72eef4cd2ad136689cb1b199843f69a
gitul0515 <gitul0515@gmail.com>
2021-07-27 18:02:04 +0900

    2021_7_27

--- Data Structure Review/12_8 radix_sort.c
+++ Data Structure Review/12_8 radix_sort.c
@@ -2,16 +2,18 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
-//-------------원형큐 코드 시작------------
-#define MAX_QUEUE_SIZE 100
+
+//---------원형큐 코드 시작---------
+#define MAX_QUEUE_SIZE 10
 typedef int element;
-typedef struct {
-	element data[MAX_QUEUE_SIZE];
+typedef struct
+{
 	int front, rear;
+	element data[MAX_QUEUE_SIZE];
 }QueueType;
 
 //오류 처리 함수 
-void error(char* message)
+void error_handling(char* message)
 {
 	fprintf(stderr, "%s\n", message);
 	exit(1);
@@ -21,7 +23,7 @@ void init_queue(QueueType* q)
 {
 	q->front = q->rear = 0;
 }
-//공백 상태 검출 함수 
+//공백 상태 검출 함수
 int is_empty(QueueType* q)
 {
 	return q->front == q->rear;
@@ -29,55 +31,55 @@ int is_empty(QueueType* q)
 //포화 상태 검출 함수
 int is_full(QueueType* q)
 {
-	return q->front == (q->rear + 1) % MAX_QUEUE_SIZE;
+	return (q->rear + 1) % MAX_QUEUE_SIZE == q->front;
 }
-//삽입 함수 
+//삽입 함수
 void enqueue(QueueType* q, element item)
 {
 	if (is_full(q))
-		error("포화 상태 오류");
+		error_handling("포화 상태 오류");
 	q->rear = (q->rear + 1) % MAX_QUEUE_SIZE;
 	q->data[q->rear] = item;
 }
-//삭제 함수 
+//삭제 함수
 element dequeue(QueueType* q)
 {
 	if (is_empty(q))
-		error("공백 상태 오류");
+		error_handling("공백 상태 오류");
 	q->front = (q->front + 1) % MAX_QUEUE_SIZE;
 	return q->data[q->front];
 }
-//피크 함수 
+//피크 함수
 element peek(QueueType* q)
 {
 	if (is_empty(q))
-		error("공백 상태 오류");
+		error_handling("공백 상태 오류");
 	return q->data[(q->front + 1) % MAX_QUEUE_SIZE];
 }
-//-------------원형큐 코드 종료---------
-//------------기수 정렬 코드 시작-----------
-#define BUCKETS 10
-#define DIGITS 4
-#define SIZE 10
+//---------원형큐 코드 끝---------
+//---------기수 정렬 코드 시작---------
+#define NUM_SIZE 10      //리스트의 요소 개수
+#define BUCKET 10        //큐의 개수
+#define DIGIT 4          //천의 자리까지 검사
 
 void radix_sort(int* list, int num)
 {
-	QueueType bucket[BUCKETS];     //10개의 큐 생성
-	int i, j, digit;
-	digit = 1;                     //일의 자리부터 정렬
+	int i, j, k;
+	int digit = 1;
+	QueueType queue[BUCKET];        //열 개의 큐를 배열로 생성
 
-	for (i = 0; i < BUCKETS; i++)
-		init_queue(&bucket[i]);    //큐들의 초기화
+	for (i = 0; i < BUCKET; i++)
+		init_queue(&queue[i]);      //각각의 큐를 초기화
 
-	while (digit <= 10)            //십의 자리까지 정렬
+	for (i = 0; i < 2; i++)
 	{
-		for (i = 0; i < num; i++)
-			enqueue(&bucket[(list[i] / digit) % 10], list[i]);
+		for (j = 0; j < num; j++)   //자릿수에 맞는 큐에 삽입
+			enqueue(&queue[(list[j] / digit) % 10], list[j]);
 
-		i = 0;
-		for (j = 0; j < BUCKETS; j++)
-			while (!is_empty(&bucket[j]))
-				list[i++] = dequeue(&bucket[j]);
+		k = 0;
+		for (j = 0; j < BUCKET; j++)  //큐에서 꺼내어 리스트에 정렬
+			while (!is_empty(&queue[j]))
+				list[k++] = dequeue(&queue[j]);
 		digit *= 10;
 	}
 }
@@ -85,15 +87,16 @@ void radix_sort(int* list, int num)
 int main()
 {
 	int i;
-	int num = SIZE;
-	int list[SIZE];
+	int num = NUM_SIZE;
+	int list[NUM_SIZE];
 	srand(time(NULL));
 	for (i = 0; i < num; i++)
 		list[i] = rand() % 100;
-	radix_sort(list, num);
+
+	radix_sort(list, num);       //기수 정렬 호출
 
 	for (i = 0; i < num; i++)
 		printf("%d ", list[i]);
 	printf("\n");
-	return 0; 
+	return 0;
 }
--- Data Structure Review/연습장.c
+++ Data Structure Review/연습장.c
@@ -1,11 +1,20 @@
-//합병 정렬 프로그램
+//버블 정렬 프로그램
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
 #define MAX_SIZE 10
+#define SWAP(X,Y,T) ((T)=(X),(X)=(Y),(Y)=(T))
 
-void merge_sort(int* list, int left, int right);
-void merge(int* list, int left, int mid, int right);
+void bubble_sort(int* list, int num)
+{
+	int i, j, temp;
+	for (i = num - 1; i > 0; i--)
+	{
+		for (j = 0; j < i; j++)
+			if (list[j] > list[j + 1]) 
+				SWAP(list[j], list[j + 1], temp);
+	}
+}
 
 int main()
 {
@@ -14,54 +23,12 @@ int main()
 	int list[MAX_SIZE];
 	srand(time(NULL));
 	for (i = 0; i < num; i++)
-		list[i] = (rand() % 100) + 1;
+		list[i] = rand() % 100;
 
-	merge_sort(list, 0, num-1);   //합병 정렬 호출
-	        					  //시작 인덱스와 마지막 인덱스 전달
+	bubble_sort(list, num);
+
 	for (i = 0; i < num; i++)
 		printf("%d ", list[i]);
 	printf("\n");
-	return 0; 
-}
-
-void merge_sort(int* list, int left, int right)
-{
-	int mid;
-	if (left < right)    //리스트의 요소가 2개 이상일 때
-	{
-		mid = (left + right) / 2;
-		merge_sort(list, left, mid);        //부분 리스트로 분할
-		merge_sort(list, mid + 1, right);
-		merge(list, left, mid, right);      //정렬된 부분리스트 합병
-	}
-}
-
-void merge(int* list, int left, int mid, int right)
-{
-	/*
-		i는 왼쪽 부분리스트의 인덱스를 가리킴
-		j는 오른쪽 부분리스트의 인덱스를 가리킴
-		k는 임시 리스트의 인덱스를 가리킴
-	*/
-	int i, j, k, l; 
-	i = left; j = mid + 1; k = left;       
-	int temp_list[MAX_SIZE] = { 0 };      //임시 리스트
-
-	while (i <= mid && j <= right)
-	{
-		if (list[i] <= list[j])
-			temp_list[k++] = list[i++];
-		else
-			temp_list[k++] = list[j++];
-	}
-
-	if (i > mid)
-		for (l = j; l <= right; l++)
-			temp_list[k++] = list[l];
-	else
-		for (l = i; l <= mid; l++)
-			temp_list[k++] = list[l];
-
-	for (l = left; l <= right; l++)
-		list[l] = temp_list[l];
+	return 0;
 }
