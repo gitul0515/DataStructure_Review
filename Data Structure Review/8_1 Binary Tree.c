e68361cf5f7295ebc72ee512b5f78393c5314320
gitul0515 <gitul0515@gmail.com>
2021-08-01 19:32:13 +0900

    2021_8_1

--- Data Structure Review/8_1 Binary Tree.c
+++ Data Structure Review/8_1 Binary Tree.c
@@ -1,4 +1,4 @@
-//이진 트리
+//연결리스트로 구현한 이진트리
 #include<stdio.h>
 #include<stdlib.h>
 #include<memory.h>
@@ -10,24 +10,20 @@ typedef struct TreeNode {
 
 int main()
 {
-	TreeNode* n1, * n2, * n3, * n4;
+	TreeNode* p;
+	TreeNode* n1, * n2, * n3;
 	n1 = (TreeNode*)malloc(sizeof(TreeNode));
 	n2 = (TreeNode*)malloc(sizeof(TreeNode));
 	n3 = (TreeNode*)malloc(sizeof(TreeNode));
-	n4 = (TreeNode*)malloc(sizeof(TreeNode));
-
 	n1->data = 10;
-	n1->llink = NULL;
-	n1->rlink = n2;
+	n1->llink = n2;
+	n1->rlink = n3;
 	n2->data = 20;
 	n2->llink = NULL;
-	n2->rlink = n3;
+	n2->rlink = NULL;
 	n3->data = 30;
-	n3->llink = n4;
+	n3->llink = NULL;
 	n3->rlink = NULL;
-	n4->data = 25;
-	n4->llink = NULL;
-	n4->rlink = NULL;
-	free(n1); free(n2); free(n3); free(n4);
-	return 0; 
+
+	free(n1); free(n2); free(n3);
 }
