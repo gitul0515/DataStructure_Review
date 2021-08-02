046f13798ce2e304e2ecaf2d17e49dc9fd92816c
gitul0515 <gitul0515@gmail.com>
2021-08-02 23:42:23 +0900

    2021_8_2

--- Data Structure Review/연습장.c
+++ Data Structure Review/연습장.c
@@ -1,33 +1,13 @@
-//이진 트리
+//시간 관련 함수: time(), Sleep()
 #include<stdio.h>
-#include<stdlib.h>
-#include<memory.h>
-
-typedef struct TreeNode {
-	int data;
-	struct TreeNode* llink, * rlink;
-}TreeNode;
+#include<time.h>         
+#include<windows.h>      
+#define MAX_SIZE 100;
 
 int main()
 {
-	TreeNode* n1, * n2, * n3, * n4;
-	n1 = (TreeNode*)malloc(sizeof(TreeNode));
-	n2 = (TreeNode*)malloc(sizeof(TreeNode));
-	n3 = (TreeNode*)malloc(sizeof(TreeNode));
-	n4 = (TreeNode*)malloc(sizeof(TreeNode));
-
-	n1->data = 10;
-	n1->llink = NULL;
-	n1->rlink = n2;
-	n2->data = 20;
-	n2->llink = NULL;
-	n2->rlink = n3;
-	n3->data = 30;
-	n3->llink = n4;
-	n3->rlink = NULL;
-	n4->data = 25;
-	n4->llink = NULL;
-	n4->rlink = NULL;
-	free(n1); free(n2); free(n3); free(n4);
-	return 0; 
+	time_t start_time = time(NULL);  //현재 시간을 저장
+	Sleep(2000);                     //프로그램 2초 정지
+	printf("%lld\n", time(NULL) - start_time);
+	return 0;
 }
