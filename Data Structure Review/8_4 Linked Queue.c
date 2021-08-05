a95ca9c37468779be243c44dc9ccf01aa5f44725
gitul0515 <gitul0515@gmail.com>
2021-08-05 16:07:20 +0900

    연결리스트로 구현한 큐

---
+++ Data Structure Review/8_4 Linked Queue.c
@@ -0,0 +1,84 @@
+//연결리스트로 구현한 큐
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef int element;         //요소의 타입
+typedef struct QueueNode {   //큐의 노드의 타입
+	element data;
+	struct QueueNode* link;
+}QueueNode;
+
+typedef struct {             //큐의 ADT 구현
+	QueueNode* front, * rear;
+}LinkedQueueType;
+
+//큐 초기화 함수 
+void init(LinkedQueueType* q)
+{
+	q->front = q->rear = 0;
+}
+//공백 상태 검출 함수 
+int is_empty(LinkedQueueType* q)
+{
+	return q->front == NULL;
+}
+//포화 상태 검출 함수 
+int is_full(LinkedQueueType* q)
+{
+	return 0;
+}
+//삽입 함수 
+void enqueue(LinkedQueueType* q, element data)
+{
+	QueueNode* temp = (QueueNode*)malloc(sizeof(QueueNode));
+	temp->data = data;
+	temp->link = NULL;
+	if (is_empty(q))       //큐가 공백이면
+	{
+		q->front = temp;
+		q->rear = temp;
+	}
+	else                   //큐가 공백이 아니면
+	{
+		q->rear->link = temp;
+		q->rear = temp;
+	}
+}
+//삭제 함수
+element dequeue(LinkedQueueType* q)
+{
+	if (is_empty(q))
+	{
+		fprintf(stderr, "큐가 공백상태입니다.");
+		exit(1);
+	}
+	QueueNode* rm = q->front;
+	element data = rm->data;
+	q->front = q->front->link;     
+	if (q->front == NULL)		 //공백 상태
+		q->rear = NULL;
+	free(rm);
+	return data;
+}
+//큐 출력 함수 
+void print_queue(LinkedQueueType* q)
+{
+	for (QueueNode* p = q->front; p != NULL; p = p->link)
+		printf("%d->", p->data);
+	printf("NULL\n");
+}
+
+//연결된 큐 테스트 함수
+int main() {
+	LinkedQueueType queue;
+
+	init(&queue);   //큐 초기화
+
+	enqueue(&queue, 1); print_queue(&queue);
+	enqueue(&queue, 2); print_queue(&queue);
+	enqueue(&queue, 3); print_queue(&queue);
+	dequeue(&queue); print_queue(&queue);
+	dequeue(&queue); print_queue(&queue);
+	dequeue(&queue); print_queue(&queue);
+	return 0; 
+}
